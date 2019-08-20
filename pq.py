18d23b38c510e65b776d77e2e41690428a6f5746
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-08-20 10:35:36 -0500

    Update pq.py

--- pq.py
+++ pq.py
@@ -1,17 +1,25 @@
 import math
 
-theta = math.radians(48)
+def checkPrime(x):
+    if x >= 2:
+        for y in range(2,x):
+            if not ( x % y ):
+                return False
+    else:
+        return False
+    return True
+
+theta = (48)
 # half angle divergence
-beta = math.radians(24)
+beta = (24)
 
 convWidth = beta * math.sqrt(2)
-n = math.pi / convWidth
+n = 180 / convWidth
 print(n)
 x = []
 
-for p in range(0,10):
-  for q in range(0,10):
-    print('{} [?] {}'.format(theta*(p+q), 1.28*n*math.pi))
-    if math.gcd(p, q) == 1 and (theta)*(p+q) > (1.28*n*math.pi):
+for p in range(0,100):
+  for q in range(0,100):
+    if p != q and checkPrime(p) and checkPrime(q) and (theta)*(p+q) > (1.28*n*180):
       x.append([p, q])
 print(x)
