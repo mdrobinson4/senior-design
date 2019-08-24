48cdd36a9a88024ac77e84447dddfcb8ace75b1a
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-08-24 12:03:55 -0500

    Update hunt.py

--- hunt.py
+++ hunt.py
@@ -18,7 +18,7 @@ GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
 GPIO.output(18, GPIO.HIGH)
 
 ser = serial.Serial(
-    port='/dev/ttyS0',
+    port='/dev/serial0',
     baudrate = 115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
