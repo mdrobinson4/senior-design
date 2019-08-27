c1ec78fe600bd9d35a231003c610a5db4a8b8662
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-08-27 16:56:47 -0500

    Create rec.py

---
+++ rec.py
@@ -0,0 +1,23 @@
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+GPIO.output(18, GPIO.HIGH)
+
+ser = serial.Serial(
+    port='/dev/serial0',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0
+)
+
+
+
+while 1:
+    try:
+        x = ser.read()
+        x = data.decode()
+        print('{}'.format(x))
+    except:
+        pass
