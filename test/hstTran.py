8822d8560cb121f198e4d46c52f6378b63c17bca
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-10-30 23:04:27 -0500

    Get the average handshake time

--- test/hstTran.py
+++ test/hstTran.py
@@ -17,18 +17,18 @@ ser = serial.Serial(
     timeout=None
 )
 
-send_time = 0
-total_time = 0
+send_time = []
+total_time = []
 
-start_time = time.time()
-str = ('1').encode()
-ser.write(str)
-send_time = time.time() - start_time
+for i in range(100):
+    start_time = time.time()
+    ser.write(('hello').encode())
+    send_time.append(time.time() - start_time)
+    res = ser.read(3)
+    data = res.decode()
+    total_time.append(time.time() - start_time)
 
-res = ser.read()
-data = res.decode()
+print('got: {}'.format(data))
+print('send time: {}'.format(sum(send_time)/len(send_time)))
+print('total handshake time: {}'.format(sum(total_time)/len(total_time)))
 
-total_time = time.time() - start_time
-
-print('It took [{}] seconds to send [{}]'.format(send_time, str))
-print('It took [{}] seconds to receive [{}]'.format(total_time, data))
