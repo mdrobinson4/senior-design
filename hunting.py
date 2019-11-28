539d20e25ecb4c3b28828bd753b9160dc61d78f1
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-11-27 19:21:19 -0600

    added handshake

--- hunting.py
+++ hunting.py
@@ -5,31 +5,117 @@ from dotenv import load_dotenv
 import numpy as np
 import time
 import RPi.GPIO as GPIO
+import serial
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 GPIO.setup(3, GPIO.OUT)
+GPIO.output(18, GPIO.HIGH)
 
-servoTheta = GPIO.PWM(3, 50)
-servoPhi = GPIO.PWM(2, 50)
-exitThread = False
-currMode = '1'
-backFlag = False
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0
+)
+
+ser.reset_input_buffer() # reset input buffer
+ser.reset_output_buffer() # reset input buffer
 
 load_dotenv()
 id = os.getenv('id')
 
+servoTheta = GPIO.PWM(3, 50)
+servoPhi = GPIO.PWM(2, 50)
+exitThread = False
+currMode = id
+backFlag = True
+
+
 def main():
     seq = generateSeq(id)
     path = pickle.load(open("path.p", "rb"))
-    servoPath(path, seq)
+    servoThread = threading.Thread(target=servoPath, args=(path, seq))
+    handshakeThread = threading.Thread(target=handshake, args=(path, seq))
+    handshakeThread.start()
+    servoThread.start()
+    servoThread.join()
+    handshakeThread.join()
+
+def handshake(path, seq):
+    global mode
+    ackWaitTime = 2*path['ackWait']
+    slotTime = path['slotTime']
+    for state in seq:
+        slotEndTime = slotTime + time.time()
+        mode = state
+        if state == '1':
+            while time.time() < slotEndTime and not exitThread:
+                checkBackFlag(slotEndTime)  # see if we are traversing the back
+                if slotEndTime - time.time() >= ackWaitTime:
+                    sendSyn()
+                    listenForAck(ackWaitTime)
+                elif slotEndTime - time.time() < ackWaitTime:
+                    time.sleep(slotEndTime - time.time())
+        elif state == '0':
+            listenForSyn(slotEndTime, ackWaitTime)
+
+def sendSyn():
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    ser.write(('hello').encode())
+
+def listenForAck(ackWaitTime):
+    global exitThread
+    endTime = ackWaitTime + time.time()
+    ser.timeout = ackWaitTime
+    x = ser.read(3)
+    try:
+        data = x.decode()
+        if data == 'ack':
+            exitThread = True
+            print('Aligned!')
+        return
+    except:
+        pass
+
+def listenForSyn(slotEndTime, ackWaitTime):
+    global exitThread
+    while time.time() < slotEndTime:
+        checkBackFlag(slotEndTime)
+        if time.time() >= slotEndTime:
+            return
+        if time.time() + ackWaitTime >= slotEndTime:
+            ser.timeout = slotEndTime - time.time()
+        else:
+            ser.timeout = ackWaitTime
+        x = ser.read(5)
+        try:
+            data = x.decode()
+            if data = 'hello'
+            ser.write(('ack').encode())
+            exitThread = True
+            print('Aligned!')
+            return
+        except:
+            pass
+
+
+
+def checkBackFlag(slotEndTime):
+    while backFlag == True and time.time() < slotEndTime:
+        pass
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
 
 def servoPath(path, seq):
     global backFlag
     (phi, theta, tranWait, recWait) = getPath(path)
-    i = 0
-    (phiRad, thetaRad) = convertValues(phi[i], theta[i])
+    i = 1
+    (phiRad, thetaRad) = convertValues(phi[0], theta[0])
     servoPhi.start(phiRad)
     servoTheta.start(thetaRad)
     while not exitThread:
